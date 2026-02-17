a0e32503882f438da39e9fea3c2b4649e21a581d
filipito93360 <brochier84@gmail.com>
2026-02-17 20:07:36 +0100

    javadoc website

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"JavaWebStore"},{"l":"JavaWebStore.exceptions"},{"l":"JavaWebStore.interfaces"},{"l":"JavaWebStore.model"},{"l":"JavaWebStore.service"},{"l":"JavaWebStore.ui"}];updateSearchResults();
